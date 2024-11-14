513745829014d9e928dc621da4a7422325203526
Robert Barrett <barrybobbit@gmail.com>
2024-11-14 13:43:55 -0500

    Create ifloop.py

---
+++ ifloop.py
@@ -0,0 +1,9 @@
+
+while True:
+  choice=input("What option do you pick. 1) McDonalds 2) Mary Browns: ")
+  if choice == "1":
+    print("BIG MAC")
+    break
+  if choice == "2":
+    print("BIG MARY")
+    break
